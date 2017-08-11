2f8993cdb221e6128282933dfb8a8127b1908fcf
nikola_kc <nikola.simunko@infinum.hr>
2017-08-11 21:19:37 +0200

    Project setup

---
+++ TravelPlaner/Common/Constants/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  TravelPlaner
+//
+//  Created by Nikola Simunko on 11/08/2017.
+//  Copyright © 2017 Nikola Simunko. All rights reserved.
+//
+
+struct Constants {
+    
+    struct StoryboardIdentifiers {
+        
+    }
+    
+    struct ViewControllerIdentifiers {
+        
+    }
+    
+    struct URLs {
+        
+    }
+    
+    struct UserDefaultsKeys {
+        
+    }
+    
+}
---
+++ TravelPlaner/Common/Networking/Router.swift
@@ -0,0 +1,9 @@
+//
+//  Router.swift
+//  TravelPlaner
+//
+//  Created by Nikola Simunko on 11/08/2017.
+//  Copyright © 2017 Nikola Simunko. All rights reserved.
+//
+
+import Foundation
