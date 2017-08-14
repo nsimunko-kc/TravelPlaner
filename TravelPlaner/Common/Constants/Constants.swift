6909cea611cea08e9071a5aff86dda2d9fe9406e
nikola_kc <nikola.simunko@infinum.hr>
2017-08-14 21:11:12 +0200

    Modules added

--- TravelPlaner/Common/Constants/Constants.swift
+++ TravelPlaner/Common/Constants/Constants.swift
@@ -9,11 +9,17 @@
 struct Constants {
     
     struct StoryboardIdentifiers {
-        
+        static let TabBar = "TabBar"
+        static let Home = "Home"
+        static let Favorites = "Favorites"
+        static let Settings = "Settings"
     }
     
     struct ViewControllerIdentifiers {
-        
+        static let TabBar = "TabBarViewController"
+        static let Home = "HomeViewController"
+        static let Favorites = "FavoritesViewController"
+        static let Settings = "SettingsViewController"
     }
     
     struct URLs {
